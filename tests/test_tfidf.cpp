c20aa7e413796f4a49eb32ee3f8739d5c5ee1c44
Hanamuke <C:\Users\Hanamuke\AppData\Local\atom\app-1.7.4\atom.exe>
2016-06-11 18:30:30 +0200

    Quelque changement sur le score idf. Je passe a 92.45% théorique.

--- tests/test_tfidf.cpp
+++ tests/test_tfidf.cpp
@@ -4,11 +4,16 @@
 #include "../utils/tfidf.hpp"
 #include <iostream>
 #include <array>
+#include <fstream>
+#include <cmath>
 
 int main()
 {
+    std::fstream out;
+    out.open("output.txt",std::fstream::out|std::fstream::trunc);
     std::vector<float> idf;
     std::vector<float> score;
+    float meanidf=0;
     //std::string word="http:";
     int size;
     Preprocessing p("../data/train.csv");
@@ -30,6 +35,15 @@ int main()
         idf[i] = idf_train(vect,i);
         //std::cout << (float)i/(float)size<<'%'<<std::endl;
     }
+    for(int i=0; i<size; i++)
+    {
+        meanidf += idf[i]/size;
+    }
+    meanidf-=1; //ca marchais mieux avec ça. Apparemment il y a des mots quand tu les utilises t'es gentil.
+    for(int i=0; i<size; i++)
+    {
+        idf[i] = abs((idf[i]-meanidf))*(idf[i]-meanidf);
+    }
     for(unsigned j=0; j<vect.size(); j++)
     {
         score[j]=tfidf_trained(vect,j,idf);
@@ -37,5 +51,12 @@ int main()
     }
     auto t=threshold(vect,score);
     std::cout << "seuil : "<<t.first<<" => score théorique :"<<t.second<<std::endl;
+    for(unsigned j=0; j<vect.size(); j++)
+    {
+        if(score[j]>=t.first)
+        out<<"1\n";
+        else
+        out<<"0\n";
+    }
     return 0;
     }
