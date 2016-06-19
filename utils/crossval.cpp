1b8d43d505323dc4e9abcc03966edbad6df113e3
Hanamuke <C:\Users\Hanamuke\AppData\Local\atom\app-1.7.4\atom.exe>
2016-06-19 14:32:16 +0200

    ajout d'un fonction de bootstrap

--- utils/crossval.cpp
+++ utils/crossval.cpp
@@ -4,6 +4,11 @@
 #include <list>
 #include <array>
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <cmath>
+#include <numeric>
+#include <random>
 std::pair<std::vector<std::vector<int>>,std::vector<std::vector<int>>> split(unsigned int n,std::vector<std::vector<int>> vect)
 {
     std::vector<std::vector<int>> train,test;
@@ -33,7 +38,7 @@ std::vector<float> crossval_idf(std::vector<std::vector<int>> vect,int dictionna
         if(sample_size==0)
         sample=vect;
         else
-        sample=split(sample_size,vect).first;
+        sample=bootstrap(sample_size,vect);
         for(int j=0; j<dictionnary_size; j++)
         idf[j][i] = idf_train(sample,j);
     }
@@ -42,4 +47,22 @@ std::vector<float> crossval_idf(std::vector<std::vector<int>> vect,int dictionna
     for(int j=0; j<dictionnary_size; j++)
     final_idf[j]=std::accumulate(idf[j].begin()+n_trash,idf[j].end()-n_trash,0);//pas besoin de moyenner ou quoi je le fait dans le main
     return final_idf;
+}
+
+std::vector<std::vector<int>> bootstrap(int n,std::vector<std::vector<int>>& vect)
+{
+    std::vector<std::vector<int>> ret;
+    int max=ret.size();
+    std::mt19937_64 rng=std::mt19937_64(time(NULL));
+    int index;
+    ret.resize(n);
+    for(int i=0; i<n; i++)
+    {
+        //j'utilise pas le modulo parce que j'ai pensé que c'était important de tirer uniformément.
+        /*tmp=(float)rand()/(float)RAND_MAX;
+        tmp=floor(n*tmp);*/
+        index=rng()%max;
+        ret[i]=vect[index];
+    }
+    return ret;
 }
--- utils/crossval.hpp
+++ utils/crossval.hpp
@@ -4,6 +4,7 @@
 #include <utility>
 
 std::pair<std::vector<std::vector<int>>,std::vector<std::vector<int>>> split(unsigned int n,std::vector<std::vector<int>> vect); //renvoie aléatoirement n échantillons, la copie est volontaire
+std::vector<std::vector<int>> bootstrap(int n,std::vector<std::vector<int>>& vect); //tire n echantillons, avec remise !
 std::vector<float> crossval_idf(std::vector<std::vector<int>> vect,int dictionnary_size,int n_sample, int n_trash,int sample_size=0); //si on laisse un paramètre à zero il prend tout.
 
 #endif
